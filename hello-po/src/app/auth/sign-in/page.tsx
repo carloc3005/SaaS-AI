449e1bebe548120b2bcd333ec95e66365d4b067f
Carlo Castillo <carlo89512@yahoo.com>
2025-07-08 15:37:17 -0700

    Rename meetai to hello-po and add auth pages
    All files and configuration were renamed from the 'meetai' directory to 'hello-po'. Added new authentication layout, sign-in, and sign-up pages, along with corresponding view components for sign-in and sign-up under the modules directory. Also updated the dev script in package.json to remove '--turbopack'.

---
+++ hello-po/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+
+const Page = () => {
+    console.log("Sign in page");
+    
+    return (
+        <SignInView />
+    );
+}
+
+export default Page;
+
