7d00e4afccd268a4b6f8b91ef84d2660a1c34080
Carlo Castillo <carlo89512@yahoo.com>
2025-07-08 01:50:04 -0700

    Integrate better-auth for authentication
    Added better-auth and related dependencies. Implemented authentication API route, client, and server logic using better-auth and drizzle adapter. Updated database schema to support user, session, account, and verification tables. Modified the home page to include a user sign-up form using the new authentication client.

--- meetai/src/app/page.tsx
+++ meetai/src/app/page.tsx
@@ -1,9 +1,39 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onError: () => {
+        window.alert("Something went wrong")
+      },
+      onSuccess: () => {
+        window.alert("Success")
+      }
+    });
+  }
+
   return (
-    <Button>
-      Click Me
-    </Button>
+    <div className="p-4 flex flex-col gap-4">
+      <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>
+        Create User
+      </Button>
+    </div>
   );
 }
+
