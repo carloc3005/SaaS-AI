3e5d56e8ba2b73b241369c706a8a58592ac07158
Carlo Castillo <carlo89512@yahoo.com>
2025-07-08 19:30:02 -0700

    Implement sign-in and sign-up pages with forms
    Added new sign-in and sign-up pages with form validation, error handling, and social login buttons. Updated SignInView and SignUpView components to include form logic using react-hook-form and zod, and improved UI layout. Added logo assets for use in authentication views.

--- hello-po/src/modules/auth/ui/views/sign-up-view.tsx
+++ hello-po/src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,12 +1,193 @@
 "use client";
 
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { OctagonAlert, OctagonAlertIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email(),
+    password: z.string().min(1, { message: "Password is required" }),
+    confirmPassword: z.string().min(1, { message: "Password is required" })
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+})
 
 export const SignUpView = () => {
-    
+    const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+    const [pending, setPending] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+    });
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        setError(null);
+        setPending(true);
+
+        authClient.signUp.email({
+            name: data.name,
+            email: data.email,
+            password: data.password,
+        },
+            {
+                onSuccess: () => {
+                    router.push("/");
+                },
+                onError: ({ error }) => {
+                    setPending(false);
+                    setError(error.message)
+                },
+            }
+        );
+
+
+    };
+
     return (
-        <Card>
-            Sign Up View
-        </Card>
+        <div className="flex min-h-screen flex-col items-center justify-center gap-10 p-6">
+            <Card className="w-full max-w-sm overflow-hidden p-0 md:max-w-3xl">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-center p-6 md:p-8">
+                            <div className="flex flex-col gap-6">
+                                <div className="flex flex-col items-center text-center">
+                                    <h1 className="text-2xl font-bold">
+                                        Let's get started
+                                    </h1>
+                                    <p className="text-muted-foreground text-balance">
+                                        Create your account
+                                    </p>
+                                </div>
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="name"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Name</FormLabel>
+                                                <FormControl>
+                                                    <Input type="text" placeholder="John Doe" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <div>
+                                    </div>
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input type="email" placeholder="carloc@example.com" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Password</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder="********" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <FormField
+                                    control={form.control}
+                                    name="confirmPassword"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Confirm Password</FormLabel>
+                                            <FormControl>
+                                                <Input type="password" placeholder="********" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                {!!error && (
+                                    <Alert className="bg-destructive/10 border-none ">
+                                        <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                                        <AlertTitle>{error}</AlertTitle>
+                                    </Alert>
+                                )}
+                                <Button disabled={pending} type="submit" className="w-full">
+                                    Sign Up
+                                </Button>
+                                <div className="after: border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center
+                                after:border-t">
+                                    <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                        or
+                                    </span>
+
+                                </div>
+                                <div className="grid grid-cols-4 gap-4">
+                                    <Button disabled={pending} variant="outline" type="button" className="w-full">
+                                        Google
+                                    </Button>
+                                    <Button disabled={pending} variant="outline" type="button" className="w-full">
+                                        Facebook
+                                    </Button>
+                                    <Button disabled={pending} variant="outline" type="button" className="w-full">
+                                        Yahoo
+                                    </Button>
+                                    <Button disabled={pending} variant="outline" type="button" className="w-full">
+                                        Github
+                                    </Button>
+                                </div>
+                                <div className="text-center text-sm">
+                                    Already have an account? <Link href="/sign-in" className="underline underline-offset-4">
+                                        Sign in
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </Form>
+
+                    <div className="bg-zinc-100 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                        <img src="/logoipsum-381.svg" alt="Image" className="h-[92px] w-[92px]" />
+                        <p className="text-2xl font-semibold text-zinc-900">
+                            Hello Po
+                        </p>
+                    </div>
+                </CardContent>
+            </Card>
+
+            <div className="text-muted-foreground *:[a]:hover:text-primary w-full max-w-sm text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4 md:max-w-3xl">
+                By clicking continue, you agree to our <a href="#">Terms of Service</a> and{" "}
+                <a href="#">Privacy Policy</a>
+            </div>
+        </div>
     );
 }
