171480021bf011e106a5a7b5bd7d098b0009ee38
Carlo Castillo <carlo89512@yahoo.com>
2025-07-11 23:49:32 -0700

    Add DiceBear avatar support and user button to sidebar
    Added @dicebear/collection and @dicebear/core dependencies for avatar generation. Introduced a new DashboardUserButton component and integrated it into the dashboard sidebar. Updated sidebar styles and logic to highlight active links and improved sidebar color variables in globals.css.

--- hello-po/src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ hello-po/src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -5,9 +5,12 @@ import {
     Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader,
     SidebarMenu, SidebarMenuButton, SidebarMenuItem
 } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
 import { BotIcon, Star, VideoIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { DashboardUserButton } from "./dashboard-user-button";
 
 const firstSection = [
     {
@@ -27,11 +30,13 @@ const secondSection = [
 ];
 
 export const DashboardSidebar = () => {
+    const pathname = usePathname();
+
     return (
         <Sidebar>
             <SidebarHeader className="text-shadow-sidebar-accent-foreground">
                 <Link href="/" className="flex items-center gap-2 px-2 pt-2">
-                    <Image src="/logo.svg" height={36} width={36} alt="Logo for Hello Po"/>
+                    <Image src="/logo.svg" height={36} width={36} alt="Logo for Hello Po" />
                     <p className="text-2xl font-semibold">Hello Po</p>
                 </Link>
             </SidebarHeader>
@@ -44,9 +49,31 @@ export const DashboardSidebar = () => {
                         <SidebarMenu>
                             {firstSection.map((item) => (
                                 <SidebarMenuItem key={item.href}>
-                                    <SidebarMenuButton>
+                                    <SidebarMenuButton asChild className={cn("h-10 hover:bg-linear-to-r/oklch border border-transparent",
+                                        "hover:border-[#5D6B68]/10 from-sidebar-accent from 5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10")} isActive={pathname === item.href}>
                                         <Link href={item.href}>
-                                            <item.icon className="size-5"/>
+                                            <item.icon className="size-5" />
+                                            <span className="text-sm font-medium tracking-tight">
+                                                {item.label}
+                                            </span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {secondSection.map((item) => (
+                                <SidebarMenuItem key={item.href}>
+                                    <SidebarMenuButton asChild className={cn("h-10 hover:bg-linear-to-r/oklch border border-transparent",
+                                        "hover:border-[#5D6B68]/10 from-sidebar-accent from 5% via-30% via-sidebar/50 to-sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10")} isActive={pathname === item.href}>
+                                        <Link href={item.href}>
+                                            <item.icon className="size-5" />
                                             <span className="text-sm font-medium tracking-tight">
                                                 {item.label}
                                             </span>
@@ -58,6 +85,9 @@ export const DashboardSidebar = () => {
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
+            <SidebarFooter className="text-white">
+                <DashboardUserButton />
+            </SidebarFooter>
         </Sidebar>
     )
 }
